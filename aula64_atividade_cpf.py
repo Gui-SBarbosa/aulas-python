ce6516ca01fc18b8bedaf750ea565cef3d21a1e9
Guilherme Sousa Barbosa <guisousabarbosa06@gmail.com>
2024-09-10 15:07:33 -0300

    cpf code

--- aula64_atividade_cpf.py
+++ aula64_atividade_cpf.py
@@ -22,8 +22,9 @@ contrrio disso:
     resutado é o valor da conta
 O primeiro dígito do CPF é 7
 """
-
-cpf = "746824890"
+#   Que codigo podre
+#   Odeio minha vida
+cpf = input("Digite os noves primeiros digitos do seu cpf: ")
 
 i = 10
 lista = []
@@ -46,4 +47,29 @@ if resultado_valor > 9:
 else:
     primeiro_valor = resultado_valor
 
-lista.append(primeiro_valor)
+lista2 = [cpf]
+lista2.append(str(primeiro_valor))
+i = 11
+
+lista3 = []
+
+for numero in lista2:
+    for numero in numero:
+        numero = int(numero)
+        resultado_2 = numero * i
+        lista3.append(resultado_2)
+        i -= 1
+
+sum_valor_2 = 0
+
+for valor2 in lista3:
+    sum_valor_2 += valor2
+
+resultado_valor2 = sum_valor_2 % 11
+
+if resultado_valor2 < 2:
+    segundo_valor = 0
+else:
+    segundo_valor = 11 - resultado_valor2
+
+print(f'{cpf}-{primeiro_valor}{segundo_valor}')
