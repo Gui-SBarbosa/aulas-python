71fd3491dad2826674de6695c19e1ea090f4caf0
Guilherme Sousa Barbosa <guisousabarbosa06@gmail.com>
2024-09-11 15:13:43 -0300

    New CPF page

--- aula65_atividade_gera_cpf.py
+++ aula65_atividade_gera_cpf.py
@@ -1,22 +1,9 @@
-import re
-import sys
+import random
 
-#   Que codigo podre
-#   Odeio minha vida
-entrada = input('CPF: ')
-cpf_enviado = re.sub(
-    r'[^0-9]',
-    '',
-    "746.824.890-70"
-    )
+cpf_9 = ''
+for i in range(9):
+    cpf_9 += str(random.randint(0, 9))
 
-entrada_e_sequencial = entrada == entrada[0] * len(entrada)
-
-if entrada_e_sequencial:
-    print("Você mandou dados sequenciais!")
-    sys.exit()
-
-cpf_9 = cpf_enviado[:9]
 i = 10
 
 resultado_digito_1 = 0
@@ -38,7 +25,4 @@ segundo_valor = segundo_valor if segundo_valor <= 9 else 0
 
 cpf_verificado = f'{cpf_9}{primeiro_valor}{segundo_valor}'
 
-if cpf_verificado == cpf_enviado:
-    print(f"{cpf_enviado} é Válido!")
-else:
-    print("CPF é Inválido!")
+print(cpf_verificado)
