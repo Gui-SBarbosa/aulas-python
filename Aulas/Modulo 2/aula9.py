27d168ffb8ff2976887a2b24ab936d2be78af836
Guilherme Sousa Barbosa <guisousabarbosa06@gmail.com>
2024-10-01 12:24:16 -0300

    update

--- Aulas/Modulo 2/aula9.py
+++ Aulas/Modulo 2/aula9.py
@@ -3,13 +3,16 @@ Closure e funções que retornam outras funções
 """
 
 
-def criar_saudacao(saudacao, nome):
-    def saudar():
-        f'{saudacao}, {nome}'
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}!'
     return saudar
 
 
-s1 = criar_saudacao('Bom Dia', 'Guilherme')
-s2 = criar_saudacao('Boa Noite', 'Guilherme')
-print(s1)
-print(s2)
+s1 = criar_saudacao('Bom Dia')
+s2 = criar_saudacao('Boa Tarde')
+s3 = criar_saudacao('Boa Noite')
+
+print(s1('Guilherme'))
+print(s2('Guilherme'))
+print(s3('Guilherme'))
